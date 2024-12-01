9370b9cb172d9c560173da93c48deb2e2474291b
rathik897 <rathik678bnm@gmail.com>
2024-12-01 16:59:45 +0530

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "git"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "gitchain"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "gt"
 )
 
 var ID ids.ID
